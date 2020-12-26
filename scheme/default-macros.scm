72abd2fe1bff1a6dcfabc04eb43b0b704dcefadb
Ultraschemer <paulosfilho@gmail.com>
2020-12-26 17:06:50 -0300

    Starting to implement javascript object call support.

--- scheme/default-macros.scm
+++ scheme/default-macros.scm
@@ -7,79 +7,51 @@
 ;;; javascript expression
 (define-macro (!x . params) `(##inline-host-expression ,@params))
 
+;;; Function application helper:
+(define 
+  (native-params-apply init-index params) 
+  (let ((sz (length params)))
+    (let loop ((i init-index) (res "") (initializer ""))
+      (if (= i sz) 
+          res
+          (loop (+ i 1) 
+                (string-append 
+                 res
+                 initializer 
+                 "g_scm2host(@" 
+                 (number->string (+ i 1)) 
+                 "@)")
+                ",")))))
+
 ;;; Statement Apply (used to call functions as statements in a more Schemey way)
 (define-macro (!sa name . args)
-  (let 
-      ((r (lambda (params) 
-            (let ((sz (length params)))
-              (let loop ((i 0) (res "") (initializer ""))
-		            (if (= i sz) 
-                    res
-                    (loop (+ i 1) 
-                          (string-append 
-                           res
-                           initializer 
-                           "g_scm2host(@" 
-                           (number->string (+ i 1)) 
-                           "@)")
-                          ",")))))))
-    (if (symbol? name) 
-	    `(##inline-host-statement 
-          ,(string-append (symbol->string name) "(" (r args) ");") 
-          ,@args)
-	    `(##inline-host-statement 
-          ,(string-append name "(" (r args) ");") 
-          ,@args))))
+  (if (symbol? name) 
+      `(##inline-host-statement 
+        ,(string-append (symbol->string name) "(" (native-params-apply 0 args) ");") 
+        ,@args)
+      `(##inline-host-statement 
+        ,(string-append name "(" (native-params-apply 0 args) ");") 
+        ,@args)))
 
 ;;; Expression Apply (used to call javascript functions as expressions in a more Schemey way)
 (define-macro (!ea name . args)
-  (let 
-      ((r (lambda (params) 
-            (let ((sz (length params)))
-              (let loop ((i 0) (res "") (initializer ""))
-		            (if (= i sz) 
-                    res
-                    (loop (+ i 1) 
-                          (string-append 
-                           res
-                           initializer 
-                           "g_scm2host(@" 
-                           (number->string (+ i 1)) 
-                           "@)")
-                          ",")))))))
-
-    (if (symbol? name) 
-	    `(##inline-host-expression 
-          ,(string-append (symbol->string name) "(" (r args) ")") 
-          ,@args)
-	    `(##inline-host-expression 
-          ,(string-append name "(" (r args) ")") 
-          ,@args))))
+  (if (symbol? name) 
+      `(##inline-host-expression 
+        ,(string-append (symbol->string name) "(" (native-params-apply 0 args) ")") 
+        ,@args)
+      `(##inline-host-expression 
+        ,(string-append name "(" (native-params-apply 0 args) ")") 
+        ,@args)))
 
 ;;; Expression Apply (used to call functions as expressions in a more Schemey way)
 (define-macro (!xa name . args)
-  (let 
-      ((r (lambda (params) 
-            (let ((sz (length params)))
-              (let loop ((i 0) (res "") (initializer ""))
-		            (if (= i sz) 
-                    res
-                    (loop (+ i 1) 
-                          (string-append 
-                           res
-                           initializer 
-                           "g_scm2host(@" 
-                           (number->string (+ i 1)) 
-                           "@)")
-                          ",")))))))
-    
-    (if (symbol? name) 
-	    `(##inline-host-expression 
-          ,(string-append (symbol->string name) "(" (r args) ")") 
-          ,@args)
-	    `(##inline-host-expression
-          ,(string-append name "(" (r args) ")") 
-          ,@args))))
+  (if (symbol? name) 
+      `(##inline-host-expression 
+        ,(string-append (symbol->string name) "(" (native-params-apply 0 args) ")") 
+        ,@args)
+      `(##inline-host-expression
+        ,(string-append name "(" (native-params-apply 0 args) ")") 
+        ,@args)))
 
 ;;; Import macro:
 (define-macro (!js-require name library)
